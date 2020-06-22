d5bff45d2afa14c502d7ff02d5987894963175c3
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-22 13:48:00 -0600

    Initial Commit for Prototype 1
    Initial Prototype 1 Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is where you gewt player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //moves vehicle forward based on player input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //rotates vehicle left/right based on player input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput* Time.deltaTime);
+    }
+}
